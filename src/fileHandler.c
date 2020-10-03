d32bc29e67c405ddb3f9b84329e2b8726845c255
DaviPtrs <davispetris@gmail.com>
2020-10-03 01:13:26 -0300

    continuação da fileToPoint

--- src/fileHandler.c
+++ src/fileHandler.c
@@ -1,5 +1,6 @@
 #include "../include/fileHandler.h"
-
+#include <string.h>
+#include <strings.h>
 char *readNewLine(FILE* input){
 	char *line;
 	long unsigned int n = 0;		
@@ -9,6 +10,7 @@ char *readNewLine(FILE* input){
 
 int countPointDimension(FILE *input){
 	char *line = readNewLine(input);
+	// printf("%s", line);
 	int m = 0;
 	strtok(line, ","); // Ignorar o identificador
 	while(strtok(NULL, ",") != NULL){
@@ -25,11 +27,36 @@ Point* fileToPoint(FILE* input){
 	rewind(input); // just in case
 
     int m = countPointDimension(input);
-	printf("%d", m);
 
 	while (!feof(input)){
 		char *line = readNewLine(input);
-		 // TODO
+		if(strcmp(line, "") == 0){ // Chegou no final do arquivo
+			break;
+		}
+		char *id;
+		double *mValues = malloc(sizeof(double) * m);
+
+		// strtok aparentemente modifica a string original, ao invés de retornar a parte dela até a vírgula
+		// se não fosse por isso, o código abaixo devia funcionar, mas nem tudo na vida são flores
+		// TODO: Arrumar essa aberração abaixo
+		id = strtok(line, ",");
+		
+		for (int i = 0; i < m; i++){
+			double buffer;
+			if(i != (m-1)){
+				buffer = atof(strtok(line, ","));
+			}else{
+				buffer = atof(strtok(line, "\n"));
+			}
+			mValues[i] = buffer;
+		}
+
+		for (int i = 0; i < m; i++){
+			printf("%.2f", mValues[i]);
+		}
+		free(line);
+		free(mValues);
 	}
+	
 	return NULL;
 }
