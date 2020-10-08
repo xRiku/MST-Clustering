92536d4ce164d8337a938ff3749d0e1e67ef162d
DaviPtrs <davispetris@gmail.com>
2020-10-08 16:43:55 -0300

    comparação pro grouping

--- src/grouping.c
+++ src/grouping.c
@@ -1,9 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "../include/grouping.h"
+#include "../include/point.h"
+
+int comparePointGroup(const void *x, const void *y){
+    Point *p1 = ((Point*)x);
+    Point *p2 = ((Point*)y);
+    char *p1SetId = getPointId(getPointSet(p1));
+    char *p2SetId = getPointId(getPointSet(p2));
+
+    int compIdSet = strcmp(p1SetId, p2SetId);
+    if(compIdSet != 0){
+        return compIdSet;
+    }else{
+        char *p1Id = getPointId(p1);
+        char *p2Id = getPointId(p2);
+        return strcmp(p1Id, p2Id);
+    }
+}
 
 
-/** Pseudo código da função de comparar pra eu não esquecer depois
- * IF id do set diferente
- *     retorna comparação de id do set
- * ELSE
- *     retorna comparação lexicográfica (q eu n sei qual é ainda)
-**/
