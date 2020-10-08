4872253cf07dcfb8e2467643425a404117c04393
DaviPtrs <davispetris@gmail.com>
2020-10-08 18:06:29 -0300

    comentei onde ta cagado

--- src/grouping.c
+++ src/grouping.c
@@ -10,13 +10,13 @@ int comparePointGroup(const void *x, const void *y){
     char *p1SetId = getPointId(getPointSet(p1));
     char *p2SetId = getPointId(getPointSet(p2));
 
-    int compIdSet = strcmp(p1SetId, p2SetId);
+    int compIdSet = strcmp(p1SetId, p2SetId); // Essa comparação ta cagando
     if(compIdSet != 0){
         return compIdSet;
     }else{
         char *p1Id = getPointId(p1);
         char *p2Id = getPointId(p2);
-        return strcmp(p1Id, p2Id);
+        return strcmp(p1Id, p2Id); // Essa aqui ta certa
     }
 }
 
