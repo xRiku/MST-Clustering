98c0f9a58eb493df247ba543feb5807069f798ce
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-10-22 07:33:34 -0300

    Removi decimal e tirei a notaçao cientifica

--- script.py
+++ script.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
-from decimal import Decimal
 import os
 import numpy as np
 import subprocess
+
+
+# Remove a notação científica
+np.set_printoptions(suppress=True)
 
 entrada3 = np.array([])
 entrada4 = np.array([])
@@ -12,8 +15,7 @@ while i < 10:
     aux = subprocess.run(['./trab1', './entradas/3.txt', '5', './saidas/3.txt'], stdout=subprocess.PIPE, universal_newlines=True)
     y = aux.stdout
     y = y.split('\n')
-    y = [Decimal(j)*1000 for j in y]
-    # print(y)
+    y = [float(j) for j in y]
     if i == 0:
         entrada3 = np.array(y)
     else:
@@ -25,7 +27,7 @@ while i < 10:
     aux = subprocess.run(['./trab1', './entradas/4.txt', '5', './saidas/4.txt'], stdout=subprocess.PIPE, universal_newlines=True)
     y = aux.stdout
     y = y.split('\n')
-    y = [Decimal(j)*1000 for j in y]
+    y = [float(j) for j in y]
     # print(y)
     if i == 0:
         entrada4 = np.array(y)
@@ -34,11 +36,11 @@ while i < 10:
     i += 1
 
 i = 0
-while i < 10:
+while i < 2:
     aux = subprocess.run(['./trab1', './entradas/5.txt', '10', './saidas/5.txt'], stdout=subprocess.PIPE, universal_newlines=True)
     y = aux.stdout
     y = y.split('\n')
-    y = [Decimal(j)*1000 for j in y]
+    y = [float(j) for j in y]
     # print(y)
     if i == 0:
         entrada5 = np.array(y)
@@ -46,6 +48,6 @@ while i < 10:
         entrada5 = np.vstack((entrada5, y))
     i += 1
 
-print(f'Médias entrada 3: {entrada3.mean(axis=0)}\n')
-print(f'Médias entrada 4: {entrada4.mean(axis=0)}\n')
-print(f'Médias entrada 5: {entrada5.mean(axis=0)}\n')
+print(f'Médias entrada 3: {entrada3.mean(axis=0)}')
+print(f'Médias entrada 4: {entrada4.mean(axis=0)}')
+print(f'Médias entrada 5: {entrada5.mean(axis=0)}')
