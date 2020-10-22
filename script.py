a2a11d67b95b0d3479998af9dafb2568166217c9
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-10-22 07:50:55 -0300

    Adicionado pandas para melhor visualizaçao

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import numpy as np
+import pandas as pd
 import subprocess
 
 
@@ -28,7 +29,6 @@ while i < 10:
     y = aux.stdout
     y = y.split('\n')
     y = [float(j) for j in y]
-    # print(y)
     if i == 0:
         entrada4 = np.array(y)
     else:
@@ -36,18 +36,28 @@ while i < 10:
     i += 1
 
 i = 0
-while i < 2:
+while i < 10:
     aux = subprocess.run(['./trab1', './entradas/5.txt', '10', './saidas/5.txt'], stdout=subprocess.PIPE, universal_newlines=True)
     y = aux.stdout
     y = y.split('\n')
     y = [float(j) for j in y]
-    # print(y)
     if i == 0:
         entrada5 = np.array(y)
     else:
         entrada5 = np.vstack((entrada5, y))
     i += 1
 
-print(f'Médias entrada 3: {entrada3.mean(axis=0)}')
-print(f'Médias entrada 4: {entrada4.mean(axis=0)}')
-print(f'Médias entrada 5: {entrada5.mean(axis=0)}')
+media3 = entrada3.mean(axis=0)
+media4 = entrada4.mean(axis=0)
+media5 = entrada5.mean(axis=0)
+
+# print(f'Médias entrada 3: {media3}')
+# print(f'Médias entrada 4: {media4}')
+# print(f'Médias entrada 5: {media5}')
+
+medias = np.array([media3,media4,media5])
+# print(medias)
+df = pd.DataFrame(medias)
+df.index = ['Entrada 3', 'Entrada 4', 'Entrada 5']
+df.columns = ['Leitura', 'Distâncias', 'Ordenação', 'MST', 'Agrupamento', 'Escrita']
+print(df)
